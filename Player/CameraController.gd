239adb9e234974df021c6b7b23fde82d6f500921
lmorao <luis.morao.dev@gmail.com>
2024-01-07 09:44:48 -0800

    Press E to place block Blocks on layer 5 and 6 New reticule to see the aiming for blocks

--- Player/CameraController.gd
+++ Player/CameraController.gd
@@ -17,6 +17,7 @@ enum CAMERA_PIVOT { OVER_SHOULDER, THIRD_PERSON }
 @onready var _camera_raycast: RayCast3D = $PlayerCamera/CameraRayCast
 
 
+
 var _aim_target : Vector3
 var _aim_collider: Node
 var _pivot: Node3D
@@ -30,77 +31,104 @@ var _euler_rotation: Vector3
 
 
 func _unhandled_input(event: InputEvent) -> void:
-	_mouse_input = event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED
-	if _mouse_input:
-		_rotation_input = -event.relative.x * mouse_sensitivity
-		_tilt_input = -event.relative.y * mouse_sensitivity
+    _mouse_input = event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED
+    if _mouse_input:
+        _rotation_input = -event.relative.x * mouse_sensitivity
+        _tilt_input = -event.relative.y * mouse_sensitivity
 
 
 func _physics_process(delta: float) -> void:
-	if not _anchor:
-		return
+    if not _anchor:
+        return
 
-	_rotation_input += Input.get_action_raw_strength("camera_left") - Input.get_action_raw_strength("camera_right")
-	_tilt_input += Input.get_action_raw_strength("camera_up") - Input.get_action_raw_strength("camera_down")
+    _rotation_input += Input.get_action_raw_strength("camera_left") - Input.get_action_raw_strength("camera_right")
+    _tilt_input += Input.get_action_raw_strength("camera_up") - Input.get_action_raw_strength("camera_down")
 
-	if invert_mouse_y:
-		_tilt_input *= -1
+    if invert_mouse_y:
+        _tilt_input *= -1
 
-	if _camera_raycast.is_colliding():
-		_aim_target = _camera_raycast.get_collision_point()
-		_aim_collider = _camera_raycast.get_collider()
-	else:
-		_aim_target = _camera_raycast.global_transform * _camera_raycast.target_position
-		_aim_collider = null
+    if _camera_raycast.is_colliding():
+        
+        _aim_target = _camera_raycast.get_collision_point()
+        _aim_collider = _camera_raycast.get_collider()
+        #print("camera colliding", _aim_collider.name)
+    else:
+        _aim_target = _camera_raycast.global_transform * _camera_raycast.target_position
+        _aim_collider = null
+        
+    if Input.is_action_just_pressed("add_block"):
+        print("adding block")
+        if is_instance_valid(_aim_collider):
+            #check if it is in layer 5=17 or 6=32
+            if _aim_collider.get_collision_layer() == 17:
+                print("Found a normal block")
+                
+            if _aim_collider.get_collision_layer() == 32:
+                print("Found a phantom block")
+                replace_phatom_with_normal(_aim_collider)
+                   
 
-	# Set camera controller to current ground level for the character
-	var target_position := _anchor.global_position + _offset
-	target_position.y = lerp(global_position.y, _anchor._ground_height, 0.1)
-	global_position = target_position
+    # Set camera controller to current ground level for the character
+    var target_position := _anchor.global_position + _offset
+    target_position.y = lerp(global_position.y, _anchor._ground_height, 0.1)
+    global_position = target_position
 
-	# Rotates camera using euler rotation
-	_euler_rotation.x += _tilt_input * delta
-	_euler_rotation.x = clamp(_euler_rotation.x, tilt_lower_limit, tilt_upper_limit)
-	_euler_rotation.y += _rotation_input * delta
+    # Rotates camera using euler rotation
+    _euler_rotation.x += _tilt_input * delta
+    _euler_rotation.x = clamp(_euler_rotation.x, tilt_lower_limit, tilt_upper_limit)
+    _euler_rotation.y += _rotation_input * delta
 
-	transform.basis = Basis.from_euler(_euler_rotation)
+    transform.basis = Basis.from_euler(_euler_rotation)
 
-	camera.global_transform = _pivot.global_transform
-	camera.rotation.z = 0
+    camera.global_transform = _pivot.global_transform
+    camera.rotation.z = 0
 
-	_rotation_input = 0.0
-	_tilt_input = 0.0
+    _rotation_input = 0.0
+    _tilt_input = 0.0
 
 
 func setup(anchor: CharacterBody3D) -> void:
-	_anchor = anchor
-	_offset = global_transform.origin - anchor.global_transform.origin
-	set_pivot(CAMERA_PIVOT.THIRD_PERSON)
-	camera.global_transform = camera.global_transform.interpolate_with(_pivot.global_transform, 0.1)
-	_camera_spring_arm.add_excluded_object(_anchor.get_rid())
-	_camera_raycast.add_exception_rid(_anchor.get_rid())
+    _anchor = anchor
+    _offset = global_transform.origin - anchor.global_transform.origin
+    set_pivot(CAMERA_PIVOT.THIRD_PERSON)
+    camera.global_transform = camera.global_transform.interpolate_with(_pivot.global_transform, 0.1)
+    _camera_spring_arm.add_excluded_object(_anchor.get_rid())
+    _camera_raycast.add_exception_rid(_anchor.get_rid())
 
 
 func set_pivot(pivot_type: CAMERA_PIVOT) -> void:
-	if pivot_type == _current_pivot_type:
-		return
+    if pivot_type == _current_pivot_type:
+        return
 
-	match(pivot_type):
-		CAMERA_PIVOT.OVER_SHOULDER:
-			_over_shoulder_pivot.look_at(_aim_target)
-			_pivot = _over_shoulder_pivot
-		CAMERA_PIVOT.THIRD_PERSON:
-			_pivot = _third_person_pivot
+    match(pivot_type):
+        CAMERA_PIVOT.OVER_SHOULDER:
+            _over_shoulder_pivot.look_at(_aim_target)
+            _pivot = _over_shoulder_pivot
+        CAMERA_PIVOT.THIRD_PERSON:
+            _pivot = _third_person_pivot
 
-	_current_pivot_type = pivot_type
+    _current_pivot_type = pivot_type
 
 
 func get_aim_target() -> Vector3:
-	return _aim_target
+    return _aim_target
 
 
 func get_aim_collider() -> Node:
-	if is_instance_valid(_aim_collider):
-		return _aim_collider
-	else:
-		return null
+    if is_instance_valid(_aim_collider):
+        return _aim_collider
+    else:
+        return null
+
+func replace_phatom_with_normal(aim_collider):
+    print("jhaha")
+    var cube_scene = preload("res://BuildingMechanic/cube.tscn")
+    var cube = cube_scene.instantiate()
+    
+    # TODO "bad way to to add this to the world
+    var parent_cube = aim_collider.get_parent().get_parent()
+    parent_cube.add_child(cube)
+    print("new cube location ", cube.global_position)
+    
+    cube.global_position = aim_collider.global_position
+    #aim_collider.queue_free()
